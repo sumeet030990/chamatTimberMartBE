9e57019b5345f74120fb0e76ccc842a5ca07653a
Sumeet Jadhav <sumeet.jadhav@e-zest.in>
2023-03-31 13:51:23 +0530

    inial commit

--- src/routes/web/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const router = Router();
-
-router.use('/home', (req: Request, res: Response) => {
-  res.send('Website Initial page');
-});
-
-export default router;
