d69be6f1c72789f41df3d18ea11b4b5aaba19bcf
Sumeet Jadhav <sumeet.jadhav@e-zest.in>
2023-04-04 16:11:07 +0530

    migration and seeding done

---
+++ prisma/migrations/20230404102905_init/migration.sql
@@ -0,0 +1,148 @@
+-- CreateTable
+CREATE TABLE `languages` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `code` VARCHAR(5) NOT NULL,
+
+    UNIQUE INDEX `languages_code_key`(`code`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `roles` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `roles_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_bank_details` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `users_id` INTEGER NOT NULL,
+    `account_name` VARCHAR(191) NOT NULL,
+    `account_number` VARCHAR(20) NOT NULL,
+    `bank_name` VARCHAR(50) NOT NULL,
+    `ifsc_code` VARCHAR(20) NOT NULL,
+    `account_type` ENUM('current', 'savings', 'salary') NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `company` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `allow_billing` BOOLEAN NOT NULL DEFAULT true,
+    `allow_gst_billing` BOOLEAN NOT NULL DEFAULT false,
+    `primary_contact` VARCHAR(15) NOT NULL,
+    `secondary_contact` VARCHAR(15) NULL,
+    `logo_url` VARCHAR(191) NULL,
+    `gst_no` VARCHAR(191) NULL,
+    `gst_tax_percentage` INTEGER NULL,
+    `address` VARCHAR(191) NULL,
+    `country_id` INTEGER NULL,
+    `country_name` VARCHAR(191) NULL,
+    `state_id` INTEGER NULL,
+    `state_name` VARCHAR(191) NULL,
+    `city_id` INTEGER NULL,
+    `city_name` VARCHAR(191) NULL,
+    `pin_code` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `allow_login` BOOLEAN NOT NULL DEFAULT true,
+    `user_name` VARCHAR(50) NULL,
+    `password` VARCHAR(191) NULL,
+    `user_role_id` INTEGER NOT NULL,
+    `language_id` INTEGER NOT NULL,
+    `primary_contact` VARCHAR(15) NULL,
+    `secondary_contact` VARCHAR(15) NULL,
+    `date_of_birth` DATETIME(3) NULL,
+    `anniversary_date` DATETIME(3) NULL,
+    `gst_no` VARCHAR(191) NULL,
+    `gst_type` ENUM('unregistered', 'registered_business_regular', 'registered_business_composition') NULL,
+    `address` VARCHAR(191) NULL,
+    `country_id` INTEGER NULL,
+    `country_name` VARCHAR(50) NULL,
+    `state_id` INTEGER NULL,
+    `state_name` VARCHAR(50) NULL,
+    `city_id` INTEGER NULL,
+    `city_name` VARCHAR(50) NULL,
+    `pin_code` VARCHAR(10) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_by` INTEGER NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `updated_by` INTEGER NULL,
+    `deleted_at` DATETIME(3) NULL,
+    `deleted_by` INTEGER NULL,
+
+    UNIQUE INDEX `users_user_name_key`(`user_name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users_company` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `company_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `task_for_user_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_by_user` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Item` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `item_code` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `user_bank_details` ADD CONSTRAINT `user_bank_details_users_id_fkey` FOREIGN KEY (`users_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_user_role_id_fkey` FOREIGN KEY (`user_role_id`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_language_id_fkey` FOREIGN KEY (`language_id`) REFERENCES `languages`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_created_by_fkey` FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_updated_by_fkey` FOREIGN KEY (`updated_by`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_deleted_by_fkey` FOREIGN KEY (`deleted_by`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users_company` ADD CONSTRAINT `users_company_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users_company` ADD CONSTRAINT `users_company_company_id_fkey` FOREIGN KEY (`company_id`) REFERENCES `company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `todo` ADD CONSTRAINT `todo_task_for_user_id_fkey` FOREIGN KEY (`task_for_user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `todo` ADD CONSTRAINT `todo_created_by_user_fkey` FOREIGN KEY (`created_by_user`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.js
@@ -0,0 +1,306 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // companies
+  await prisma.company.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: 'Chamat Timber Mart',
+      allow_billing: true,
+      allow_gst_billing: false,
+      primary_contact: '9595109394',
+      gst_no: '11111111',
+      gst_tax_percentage: 18,
+      address: 'Queta Colony',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: 440008,
+    },
+  });
+
+  await prisma.company.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      name: 'Chamat Furniture Factory',
+      allow_billing: false,
+      allow_gst_billing: true,
+      primary_contact: '9595109394',
+      gst_no: '22222222',
+      gst_tax_percentage: 18,
+      address: 'Queta Colony',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: 440008,
+    },
+  });
+
+  // roles
+  await prisma.roles.upsert({
+    where: { slug: 'admin' },
+    update: {},
+    create: {
+      name: 'Admin',
+      slug: 'admin',
+    },
+  });
+
+  await prisma.roles.upsert({
+    where: { slug: 'customer_user' },
+    update: {},
+    create: {
+      name: 'Customer User',
+      slug: 'customer_user',
+    },
+  });
+
+  await prisma.roles.upsert({
+    where: { slug: 'customer_company' },
+    update: {},
+    create: {
+      name: 'Customer Company',
+      slug: 'customer_company',
+    },
+  });
+
+  await prisma.roles.upsert({
+    where: { slug: 'employee_operator' },
+    update: {},
+    create: {
+      name: 'Employee Operator',
+      slug: 'employee_operator',
+    },
+  });
+
+  await prisma.roles.upsert({
+    where: { slug: 'employee_normal' },
+    update: {},
+    create: {
+      name: 'Employee Normal',
+      slug: 'employee_normal',
+    },
+  });
+
+  // Language
+  await prisma.languages.upsert({
+    where: { code: 'en' },
+    update: {},
+    create: {
+      name: 'English',
+      code: 'en',
+    },
+  });
+
+  await prisma.languages.upsert({
+    where: { code: 'hi' },
+    update: {},
+    create: {
+      name: 'Hindi',
+      code: 'hi',
+    },
+  });
+
+  await prisma.languages.upsert({
+    where: { code: 'mr' },
+    update: {},
+    create: {
+      name: 'Marathi',
+      code: 'mr',
+    },
+  });
+
+  // users
+  await prisma.users.upsert({
+    where: { user_name: 'sumeet' },
+    update: {},
+    create: {
+      name: 'sumeet',
+      allow_login: true,
+      user_name: 'sumeet',
+      password: 'password',
+      user_role_id: 1,
+      language_id: 1,
+      primary_contact: '8408880505',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: '440008',
+      created_by: 1,
+    },
+  });
+
+  await prisma.users.upsert({
+    where: { user_name: 'pratiek' },
+    update: {},
+    create: {
+      name: 'pratiek',
+      allow_login: true,
+      user_name: 'pratiek',
+      password: 'password',
+      user_role_id: 1,
+      language_id: 1,
+      primary_contact: '9595109394',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: '440008',
+      created_by: 1,
+    },
+  });
+
+  await prisma.users.upsert({
+    where: { user_name: 'customer_company' },
+    update: {},
+    create: {
+      name: 'Customer Company',
+      allow_login: true,
+      user_name: 'customer_company',
+      password: 'password',
+      user_role_id: 3,
+      language_id: 1,
+      primary_contact: '0123456789',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: '440008',
+      created_by: 1,
+    },
+  });
+
+  await prisma.users.upsert({
+    where: { user_name: 'employee_operator' },
+    update: {},
+    create: {
+      name: 'Employee Operator',
+      allow_login: true,
+      user_name: 'employee_operator',
+      password: 'password',
+      user_role_id: 4,
+      language_id: 1,
+      primary_contact: '0123456789',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: '440008',
+      created_by: 1,
+    },
+  });
+
+  await prisma.users.upsert({
+    where: { user_name: 'employee_normal' },
+    update: {},
+    create: {
+      name: 'Employee Normal',
+      allow_login: true,
+      user_name: 'employee_normal',
+      password: 'password',
+      user_role_id: 5,
+      language_id: 1,
+      primary_contact: '0123456789',
+      country_id: 101,
+      country_name: 'India',
+      state_id: 4008,
+      state_name: 'Maharashtra',
+      city_id: 133116,
+      city_name: 'Nagpur',
+      pin_code: '440008',
+      created_by: 1,
+    },
+  });
+
+  // user company mapping
+  await prisma.users_company.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      user_id: 1,
+      company_id: 1,
+    },
+  });
+  await prisma.users_company.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      user_id: 1,
+      company_id: 2,
+    },
+  });
+
+  await prisma.users_company.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      user_id: 2,
+      company_id: 1,
+    },
+  });
+
+  await prisma.users_company.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      user_id: 2,
+      company_id: 2,
+    },
+  });
+
+  await prisma.users_company.upsert({
+    where: { id: 5 },
+    update: {},
+    create: {
+      user_id: 3,
+      company_id: 1,
+    },
+  });
+  await prisma.users_company.upsert({
+    where: { id: 6 },
+    update: {},
+    create: {
+      user_id: 4,
+      company_id: 1,
+    },
+  });
+  await prisma.users_company.upsert({
+    where: { id: 7 },
+    update: {},
+    create: {
+      user_id: 5,
+      company_id: 1,
+    },
+  });
+}
+main()
+  .then(async () => {
+    // eslint-disable-next-line no-console
+    console.log('db seed completed');
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
