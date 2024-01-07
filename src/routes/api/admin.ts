78ac2314a6ed77958d4281aaf7ce24d96e8ef5c3
Sumeet Jadhav <sumeet.jadhav@e-zest.in>
2024-01-08 01:42:43 +0530

    commented code for creating public private key

--- src/routes/api/admin.ts
+++ src/routes/api/admin.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import AdminController from '../../app/Controllers/AdminController';
+import protectedRoutes from '../../app/Middlewares/protectedRouteMiddleware';
 
 const router = Router();
 
-router.get('', [], AdminController.generatePublicPrivateKeys);
+router.get('', [protectedRoutes], AdminController.generatePublicPrivateKeys);
 
 export default router;
